19e1a155484112dc8f565b40d38c4a85a1023b9c
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-12 18:12:29 +0000

    Freeing the buffer

--- get_f.c
+++ get_f.c
@@ -25,6 +25,7 @@ void get_f(stack_t **list, char *buffer, unsigned int line)
 
 	token = strtok(buffer, " \n");
 	number = strtok(NULL, " \n");
+	free_pointer(buffer);
 	buffer = NULL;
 
 	for (i = 0; ops[i].opcode != NULL && token != NULL; i++)
@@ -41,7 +42,7 @@ void get_f(stack_t **list, char *buffer, unsigned int line)
 			return;
 		}
 	}
-	
+
 	fclose(file);
 	free_list(list);
 	file_error(token);
